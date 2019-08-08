63d4ae0591c271b7452727970abcaafba39ae1de
Erislândio Soares <erislandio.soares@acct.global>
2019-08-07 23:54:22 -0300

    add authentication route jwt test

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -1,7 +1,36 @@
+import generateToken from "../helpers/generateToken";
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 module.exports = {
     async authenticate(req, res) {
-        console.log(req.body);
+        try {
+            const { email, password } = req.body;
 
-        return;
+            const user = await User.findOne({ email }).select("+password");
+
+            if (!user) {
+                return res.status(404).send({
+                    code: 2,
+                    message: "User is not registered!"
+                });
+            }
+
+            if (!(await bcrypt.compare(password, user.password))) {
+                return res.status(400).send({
+                    code: 3,
+                    message: "Incorrect Password"
+                });
+            }
+
+            user.password = undefined;
+
+            return res.status(200).send({
+                user,
+                token: generateToken({ id: user.email })
+            });
+        } catch (error) {
+            return res.status(500).send(error);
+        }
     }
 };
