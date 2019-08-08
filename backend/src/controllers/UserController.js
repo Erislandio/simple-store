ae76b0d013c425626484a27d3757de359e8ac5d5
Erislândio Soares <erislandio.soares@acct.global>
2019-08-07 23:24:15 -0300

    add controllers new user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,8 +2,25 @@ const User = require("../models/User");
 
 module.exports = {
     async store(req, res) {
-        console.log(req.body);
+        try {
+            const {
+                body: { email }
+            } = req;
 
-        return req.body;
+            const user = await User.findOne({ email });
+            console.log(user);
+            if (user) {
+                return res.status(200).send({
+                    code: 1,
+                    message: "user already exists!"
+                });
+            }
+
+            const newUser = await User.create(req.body);
+
+            return res.status(201).send(newUser);
+        } catch (error) {
+            return res.status(500).send(error);
+        }
     }
 };
