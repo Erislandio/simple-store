668435eec4f201f80b66fef859a7533674acc064
Erislândio Soares <erislandio.soares@acct.global>
2019-08-07 22:50:36 -0300

    add server simple template

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const chalk = require("chalk");
+const port = 3333;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+    return res.send({
+        ok: true
+    });
+});
+
+app.listen(port, () => {
+    console.log(chalk.bgCyan(`Server running in http://localhost:${port}/`));
+});
