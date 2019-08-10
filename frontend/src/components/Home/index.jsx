16831764b295969ca78d0efd026cac0d68b29319
erislandio <erislandio.soares@acct.global>
2019-08-10 17:36:29 -0300

    add template basic react

---
+++ frontend/src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                horm
+            </div>
+        );
+    }
+}
+
+export default Home;
